303b704fbe6f8b454a5fecc804fe0ab8ef2c6e1a
Rowan Hisham <rowan.hisham133@gmail.com>
2019-04-29 23:09:34 +0200

    type strategy

--- src/levels/strategies/TypeStrategy.java
+++ src/levels/strategies/TypeStrategy.java
@@ -1,36 +1,72 @@
 package levels.strategies;
 
+import java.util.Collection;
 import java.util.List;
 
 import characters.Character;
 import characters.Character.CharacterType;
 import commands.MoveCommand;
 import levels.Level;
+import levels.Level.ShipSide;
 
 public class TypeStrategy extends AbstractStrategy {
     @Override
     public boolean validMove() {
-    	List<Character> characterList = Level.getInstance().getShip().getOnBoard();
     	boolean containHero = false;
     	boolean containVillain = false;
     	boolean containPassive = false;
     	boolean containDoubleAgent = false;
-    	boolean containCustom = false;
     	
-    	for( Character character : characterList) {
+    	Collection<Character> onBoard = Level.getInstance().getShip().getOnBoard();
+    	Collection<Character> leftCharacters = Level.getInstance().getLeftCharacters();
+    	Collection<Character> rightCharacters = Level.getInstance().getRightCharacters();
+    	
+    	if( Level.getInstance().getShipSide() == ShipSide.LEFT)
+    		rightCharacters.addAll(onBoard);
+    	else
+    		leftCharacters.addAll(onBoard);
+    	
+    	
+    	for( Character character : rightCharacters) {
     		switch (character.getType()){
     		case HERO : containHero = true; break;
     		case VILLAIN: containVillain = true; break;
     		case PASSIVE: containPassive = true; break;
     		case DOUBLE_AGENT: containDoubleAgent = true; break;
-    		case CUSTOM: containCustom = true; break;
     		}
+    		if(containDoubleAgent)
+    			break;
     	}
     	
-    	if( (containHero && containVillain) || (containVillain && containPassive) || !Level.getInstance().getShip().hasPilot())
-    		return false;
-    	else 
-    		return true;
+
+    	if(Level.getInstance().getShipSide() == ShipSide.LEFT ) {
+    		if(!containDoubleAgent)
+    			return false;
+    	}else {
+    		if( (containHero && containVillain) || (containVillain && containPassive) )
+        		return false;
+    	}
     	
+    	
+    	for( Character character : leftCharacters) {
+    		switch (character.getType()){
+    		case HERO : containHero = true; break;
+    		case VILLAIN: containVillain = true; break;
+    		case PASSIVE: containPassive = true; break;
+    		case DOUBLE_AGENT: containDoubleAgent = true; break;
+    		}
+    		if(containDoubleAgent)
+    			break;
+    	}
+    	
+    	if(Level.getInstance().getShipSide() == ShipSide.RIGHT ) {
+    		if(!containDoubleAgent)	
+    			return false;
+    	}else {
+    		if( (containHero && containVillain) || (containVillain && containPassive) )
+        		return false;
+    	}
+    	
+    	return true;
     }
 }
