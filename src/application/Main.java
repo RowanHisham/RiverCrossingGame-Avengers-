6d4d232a3fab18abfe970079bc4ff3a21e3d7636
Rowan Hisham <rowan.hisham133@gmail.com>
2019-04-30 23:44:30 +0200

    level gui

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,23 +1,56 @@
 package application;
 	
+import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.image.Image;
 
 
 public class Main extends Application {
+	private Media sound;
+	private MediaPlayer mediaPlayer;
 	
 	@Override
 	public void start(Stage primaryStage) throws IOException {
-            
+		String musicFile = "Avengers Suite (Theme).mp3";     // For example
+		sound = new Media(new File(musicFile).toURI().toString());
+		 mediaPlayer = new MediaPlayer(sound);
+		 System.out.println(sound.getDuration());
+		 mediaPlayer.setOnEndOfMedia(new Runnable() {
+		       public void run() {
+		    	   mediaPlayer.seek(Duration.ZERO);
+		       }
+		   });
+		 mediaPlayer.setOnReady(new Runnable() {
+
+		        @Override
+		        public void run() {
+
+		            System.out.println("Duration: "+sound.getDuration().toSeconds());
+
+		            // display media's metadata
+		            for (Map.Entry<String, Object> entry : sound.getMetadata().entrySet()){
+		                System.out.println(entry.getKey() + ": " + entry.getValue());
+		            }
+
+		            // play if you want
+		            mediaPlayer.setVolume(0.15);
+		            mediaPlayer.stop();
+		            mediaPlayer.play();
+		        }
+		    });
            
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("MainGameForm.fxml"));
--- src/application/MainGameFormController.java
+++ src/application/MainGameFormController.java
@@ -1,10 +1,13 @@
 package application;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+
+import com.jfoenix.controls.JFXButton;
 
 import javafx.animation.PauseTransition;
 import javafx.event.ActionEvent;
@@ -16,14 +19,19 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
-
+import levels.Level;
+import characters.*;
+import characters.Character;
 public class MainGameFormController {
-
+	private Media sound;
+	private MediaPlayer mediaPlayer;
 	
 	@FXML
-	private ImageView  imgLeftAnim1, imgLeftAnim2, imgLeftAnim3,imgRightAnim1, imgRightAnim2, imgRightAnim3;
+	private ImageView  imgLeftAnim1, imgLeftAnim2, imgLeftAnim3,imgRightAnim1, imgRightAnim2, imgRightAnim3,imgTopLeft,imgTopRight;
 	
 	@FXML
 	private Rectangle rectangle;
@@ -34,10 +42,18 @@ public class MainGameFormController {
 	@FXML
 	private HBox shipLeftGroup, shipRightGroup;
 	
+	@FXML
+	private JFXButton btn_cross;
+	
+	@FXML
+	private Pane pn_Warning;
+	
 	private Map<ImageView,Character> leftShipMap = new LinkedHashMap<>();
 	private Map<ImageView,Character> rightShipMap = new LinkedHashMap<>();
 	private Map<ImageView,Character> leftCharMap = new LinkedHashMap<>();
 	private Map<ImageView,Character> rightCharMap = new LinkedHashMap<>();
+	
+	boolean shipLeft = true;
 
 
 	@FXML
@@ -50,6 +66,8 @@ public class MainGameFormController {
 		imgRightAnim3.setVisible(false);
 		rectangle.setVisible(false);
 		
+		pn_Warning.setVisible(false);
+		
 		
 		for(Node node: shipLeftGroup.getChildren()) {
 			leftShipMap.put((ImageView) node, null);
@@ -60,8 +78,30 @@ public class MainGameFormController {
 			rightShipMap.put((ImageView) node, null);
 			node.setVisible(false);
 		}
-	}
 
+		
+		for(int i=0; i < rightCharGroup.getChildren().size(); i++ ) {
+//			for(Character node: Level.getInstance().getLeftCharacters()) {
+//				rightCharMap.put((ImageView)rightCharGroup.getChildren().get(i), node);
+//				((ImageView)rightCharGroup.getChildren().get(i)).setVisible(false);
+//				i++;
+//			}
+			rightCharMap.put((ImageView)rightCharGroup.getChildren().get(i), null);
+			((ImageView)rightCharGroup.getChildren().get(i)).setVisible(false);
+
+		}
+		
+		for(int i=0; i < leftCharGroup.getChildren().size(); i++ ) {
+//			for(Character node: Level.getInstance().getLeftCharacters()) {
+//				leftCharMap.put((ImageView)leftCharGroup.getChildren().get(i), node);
+//				((ImageView)leftCharGroup.getChildren().get(i)).setVisible(true);
+//				i++;
+//			}
+			leftCharMap.put((ImageView)leftCharGroup.getChildren().get(i), null);
+			((ImageView)leftCharGroup.getChildren().get(i)).setVisible(true);
+		}
+	}
+	
 
 	@FXML
 	void buttonOnAction(Event event) throws IOException {
@@ -109,6 +149,9 @@ public class MainGameFormController {
 			rightShipAnimation((ImageView)shipRightGroup.getChildren().get(3));
 		}else if(event.getSource() == shipRightGroup.getChildren().get(4)) {
 			rightShipAnimation((ImageView)shipRightGroup.getChildren().get(4));
+		}else if(event.getSource() == btn_cross) {
+			System.out.println("here");
+			animateShip();
 		}
 	} 
 
@@ -242,5 +285,58 @@ public class MainGameFormController {
 			}
 		}
 		
+	}
+	
+	void animateShip() {
+		invalidMoveAnimation();
+		
+		 
+		if(shipLeft) {
+			Image image = new Image("/helicarrierLeftEmpty.png");
+			imgTopLeft.setImage(image);
+			image = new Image("/helicarrierRightAvailable.png");
+			imgTopRight.setImage(image);	
+			shipLeft = false;
+		}else {
+			Image image = new Image("/helicarrierLeftAvailable.png");
+			imgTopLeft.setImage(image);
+			image = new Image("/helicarrierRightEmpty.png");
+			imgTopRight.setImage(image);
+			shipLeft = true;
+		}
+	}
+	
+	void invalidMoveAnimation() {
+		String musicFile = "nick.mp3";     // For example
+		sound = new Media(new File(musicFile).toURI().toString());
+		 mediaPlayer = new MediaPlayer(sound);
+		 System.out.println(sound.getDuration());
+		
+		 mediaPlayer.setOnReady(new Runnable() {
+
+		        @Override
+		        public void run() {
+
+		            System.out.println("Duration: "+sound.getDuration().toSeconds());
+
+		            // display media's metadata
+		            for (Map.Entry<String, Object> entry : sound.getMetadata().entrySet()){
+		                System.out.println(entry.getKey() + ": " + entry.getValue());
+		            }
+
+		            // play if you want
+		            mediaPlayer.stop();
+		            mediaPlayer.play();
+		            pn_Warning.setVisible(true);
+		        }
+		    });
+		 PauseTransition pause = new PauseTransition(Duration.seconds(7));
+			pause.setOnFinished(new EventHandler<ActionEvent>() {
+				@Override public void handle(ActionEvent t) {
+					pn_Warning.setVisible(false);
+				}
+			}
+					);
+			pause.play();
 	}
 }
